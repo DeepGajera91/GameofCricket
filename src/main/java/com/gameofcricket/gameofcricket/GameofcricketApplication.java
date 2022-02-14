491031eff8f4426fd2221a3ba32c54e881a4e261
Deep <deepgajera91@gmail.com>
2022-02-14 13:46:48 +0530

    initial commit

---
+++ src/main/java/com/gameofcricket/gameofcricket/GameofcricketApplication.java
@@ -0,0 +1,13 @@
+package com.gameofcricket.gameofcricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameofcricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameofcricketApplication.class, args);
+	}
+
+}
